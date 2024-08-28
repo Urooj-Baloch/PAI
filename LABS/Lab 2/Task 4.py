82f4cf27a419992901e29347f67796ddfda92071
Urooj Baloch <144048378+Urooj-Baloch@users.noreply.github.com>
2024-08-28 10:40:36 +0500

    Add files via upload

---
+++ LABS/Lab 2/Task 4.py
@@ -0,0 +1,9 @@
+def employee(name, sal=10000):
+    tax = sal * 0.02
+    after_tax = sal - tax
+    print(f"Employee Name: ",name)
+    print(f"Salary after tax: ",after_tax)
+
+
+employee("Urooj Baloch")
+employee("Mahnoor Hussain", 15000)  # With specified salary
